7f8fa752df6234b9a814d2171472449111a54eb2
shalva330 <kochishvilishako40@gmail.com>
2025-02-22 19:07:56 +0400

    commit

---
+++ homework/hw4.py
@@ -0,0 +1,9 @@
+name = "shako"
+surname = "yochishvili"
+city = "kaspi"
+age = 16
+
+
+print("i'am " + name  + surname) 
+print("i live in " +  city )
+print("i'm "  +  str(age) + (" old"))
---
+++ hw6.py
@@ -0,0 +1,23 @@
+surname = "yochishvili"
+name = "shako"
+city = "kaspi"
+
+weight = 60
+height = 180
+age= 16
+
+one1=10.5
+two2=15.5
+three3=5.5
+
+print(name)
+print(surname)
+print(city)
+
+print(height)
+print(weight)
+print(age)
+
+print(one1)
+print(two2)
+print(three3)
