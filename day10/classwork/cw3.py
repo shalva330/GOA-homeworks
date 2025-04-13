14081af3ac17ba26036eabfc02d2866090ae62b6
shalva330 <kochishvilishako40@gmail.com>
2025-04-13 20:34:24 +0400

    commit

---
+++ day10/classwork/cw3.py
@@ -0,0 +1,10 @@
+num1= int(input("Enter number1"))
+num2= int(input("Enter number1"))
+
+
+print("num1" > "num2")
+print("num1" < "num2")
+print("num1" == "num2")
+print("num1" != "num2")
+print("num1" <= "num2")
+print("num1" >= "num2")
